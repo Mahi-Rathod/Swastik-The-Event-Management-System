61449b25db8b24b3dbf3bffeaf54205252aebce1
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-03-20 11:50:07 +0530

    changed in django Models

--- Server/DJangoServer/BackEnd/Accounts/models.py
+++ Server/DJangoServer/BackEnd/Accounts/models.py
@@ -2,12 +2,26 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # class CustomerAccounts(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length  = 100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class vendor(models.Model):
+    pass
+
 class Products(models.Model):
-    title = models.CharField(max_length = 100)
+    name        = models.CharField(max_length = 100)
     description = models.CharField(max_length = 100)
-    price = models.CharField(max_length = 100)
-    category = models.CharField(max_length = 100)
-    image = models.ImageField(upload_to='images/', default="")
+    price       = models.CharField(max_length = 100)
+    image       = models.ImageField(upload_to='images/', default="")
+    category    = models.ForeignKey(Category, on_delete = models.CASCADE)
+    owner       = models.ForeignKey(Category, on_delete  = models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
+
+class Customer(models.Model):
+    pass
+
