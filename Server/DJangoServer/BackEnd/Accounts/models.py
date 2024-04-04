c079846ea0e36993c792679dc4f49a636f05a6ee
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-04-04 14:52:34 +0530

    created models

--- Server/DJangoServer/BackEnd/Accounts/models.py
+++ Server/DJangoServer/BackEnd/Accounts/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
 class Vendor(models.Model):
     pass
 
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
 class Product(models.Model):
     name        = models.CharField(max_length = 100)
     description = models.CharField(max_length = 100)
@@ -18,12 +21,24 @@ class Product(models.Model):
     image       = models.ImageField(upload_to='images/', default="")
     category    = models.ForeignKey(Category, on_delete = models.CASCADE)
     sold        = models.IntegerField(default = 0)
+    # location    = models.ForeignKey(Location, on_delete= models.CASCADE) 
     # owner       = models.ForeignKey(Vendor, on_delete  = models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
 
+class Food(models.Model):
+    pass
+
+class Venue(models.Model):
+    
+    venues = {
+        'mumbai' : ['a', 'b', 'c', 'd'],
+    }
 
 class Customer(models.Model):
+    pass
+
+class Order(models.Model):
     pass
 
