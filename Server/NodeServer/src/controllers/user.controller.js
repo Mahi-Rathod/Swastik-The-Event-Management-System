7eea9e34c6f103a21df9fb73bfc26eadd28bb92f
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-04-09 17:28:51 +0530

    designed login functionality

--- Server/NodeServer/src/controllers/user.controller.js
+++ Server/NodeServer/src/controllers/user.controller.js
@@ -1,9 +1,28 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+//Generate Access and refresh Token
+
+const generateAccessAndRefreshToken = async(userId) =>{
+    try{
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+    }
+    catch(error){
+        throw new ApiError(500, "Something went wrong while Generating access or refresh token")
+    }
+}
+
+
+//Controller for Registering the Users
 const registerUser = asyncHandler (async(req, res)=>{
 
     const {fullName, email, mobileNumber, password} =req.body
@@ -22,27 +41,9 @@ const registerUser = asyncHandler (async(req, res)=>{
     if(existedUser){
         throw new ApiError(409, "User with email or phone exist");
     }
-    
-    //for avatar
-    //something
-    // const avatarLocalPath = req.files?.avatar[0]?.path
-    // const coverImageLocalPath = req.files?.coverImage[0]?.path
-
-    // if(!avatarLocalPath){
-    //     throw new ApiError(400, "Avatar File is Required");
-    // }
-
-    // const avatar = await uploadOnCloudinary(avatarLocalPath)
-    // const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
-    // if(!avatar){
-    //     throw new ApiError(400, "Avatar File is Required");
-    // }
 
     const user = await User.create({
         fullName,
-        // avatar : avatar.url,
-        // coverImage: coverImage?.url || "" ,
         email,
         mobileNumber,
         password
@@ -60,4 +61,56 @@ const registerUser = asyncHandler (async(req, res)=>{
 
 })
 
-export {registerUser}
+//Controller for Login User
+const loginUser = asyncHandler(async(req, res)=>{
+    
+    const {email, mobileNumber, password} = req.body
+
+    if(!(email || mobileNumber)){
+        throw new ApiError(400, "Email or Mobile Number Required")
+    }
+
+    const user = await User.findOne({
+        $or:[{email}, {mobileNumber}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User Does not Exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Password is invalid")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user?._id)
+    const loggedInUser = await User.findById(user?._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure  : true 
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user:loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User Logged In Successfully"
+            )
+        )
+    
+})
+
+export {
+    registerUser,
+
+}
