5cfd22c12ff41070c496536e44fe49e2f51ff9d1
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-04-20 11:34:59 +0530

    started Designing of booking page

--- FrontEnd/Swastik-The-Event-Management-System/src/components/BookPackages/BookPackage.jsx
+++ FrontEnd/Swastik-The-Event-Management-System/src/components/BookPackages/BookPackage.jsx
@@ -1,76 +1,55 @@
-import img1 from '../../assets/gallary/img1.jpg'
-import './BookPackage.css'
 import React, { useState } from 'react';
-// import Calendar from 'react-calendar';
-// import 'react-calendar/dist/Calendar.css';
-function Book(){
+import img1 from "../../assets/birthday/birthday_1.jpeg"
+function Book() {
     const [details, setDetails] = useState([
         {
-            Locations :"Delhi",
-            Date : Date(),
+            Locations: "Delhi",
+            Date: Date(),
             Guest: 10,
             Rooms: 5,
-            flight:{
-                from:"Aurangabad",
-                to  :"Delhi"
+            flight: {
+                from: "Aurangabad",
+                to: "Delhi"
             },
-            Food :[],
-            Decoration :[],
+            Food: [],
+            Decoration: [],
         }
     ])
-    const change = ({key, value}) =>{
+    const change = ({ key, value }) => {
         setDetails(key.value)
     }
     const eventLocations = ['Nanded', 'Pune', 'Sambhajinagar', 'Delhi', 'Jaipur', 'utii', 'Bengaluru']
-    return(
-        <>
-            <div className='image' style={{ backgroundImage: `url(${img1})` }} />
-            <div className='offers'></div>
-            <form>
-                <div className="event-locations">
-                    <p className="text-black mb-2 font-bold my-1">Locations</p>
-                    <select
-                        className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    >
-                        {eventLocations.map((Location)=>(
-                            <option key={Location} value={Location}>
-                                {Location}
-                            </option>
-                        ))}
-                    </select>
-                    <label htmlFor="dates" className="text-black mb-2 font-bold my-1">Date</label>
-                    <input type='date' id='dates' className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none" />
 
-                </div>
-                <table className='table'>
-                    <thead className=''>
-                        <tr>
-                            <th className='head'><div> Offerings </div></th>
-                            <th className='head'><div>Prices</div></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr> 
-                            <td className='head'><div className='td'>Private Resort:Guest, Rooms</div></td>
-                            <td className='head'><div className='td'>500000</div></td>
-                        </tr>
-                    
-                        <tr>
-                            <td className='head'><div className='td'>Food</div></td>
-                            <td className='head'><div className='td'>30000(may change)</div></td>
-                        </tr>
-                        <tr>
-                            <td className='head'><div className='td'>Decoration</div></td>
-                            <td className='head'><div className='td'>300000</div></td>
-                        </tr>
-                        <tr>
-                            <td className='head'><div className='td'>Functions,haldi,mehandi</div></td>
-                            <td className='head'><div className='td'>All The More.</div></td>
-                        </tr>
+    const images = [img1, img1, img1, img1, img1]
+    return (
+        <>
+            <div className="bg-white w-[70%] h-[90vh] m-auto mt-5 shadow-xl rounded-md flex justify-evenly">
+                <div className="flex flex-col p-2 gap-3 w-[50%]">
+                    <div className="h-[70%] p-8 w-[100%] bg-white shadow-md rounded-md flex justify-center">
+                        <img src={img1} alt="" className='h-[100%] rounded-md shadow-lg' />
+                    </div>
+
+                    <div className="h-[30%] w-[100%] gap-5 flex flex-row items-center">
+                        <div className='w-[50%] h-[50%] p-4 rounded-md'>
+                            <img src={img1} alt="" className='w-[100%] h-[100%] shadow-lg  rounded-md' />
+                        </div>
+                        <div className='w-[50%] h-[50%] shadow-lg p-4 rounded-md'>
+                            <img src={img1} alt="" className='w-[100%] h-[100%] shadow-lg  rounded-md' />
+                        </div>
+                        <div className='w-[50%] h-[50%] shadow-lg p-4 rounded-md'>
+                            <img src={img1} alt="" className='w-[100%] h-[100%] shadow-lg  rounded-md' />
+                        </div>
+                        <div className='w-[50%] h-[50%] shadow-lg p-4 rounded-md'>
+                            <img src={img1} alt="" className='w-[100%] h-[100%] shadow-lg  rounded-md' />
+                        </div>
                         
-                    </tbody>
-                </table>
-            </form>
+                    </div>
+                </div>
+
+                <div className="t">
+                    <h3> Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                </div>
+            </div>
         </>
     )
 }
