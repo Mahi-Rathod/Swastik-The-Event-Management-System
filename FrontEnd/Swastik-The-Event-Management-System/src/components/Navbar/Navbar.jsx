1179f0d817cd10b8c28d6ee1f8226bbc6d24567d
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-05-19 22:15:14 +0530

    update

--- FrontEnd/Swastik-The-Event-Management-System/src/components/Navbar/Navbar.jsx
+++ FrontEnd/Swastik-The-Event-Management-System/src/components/Navbar/Navbar.jsx
@@ -23,7 +23,6 @@ function Navbar() {
             try {
                 const response = await axiosInstance.get('/getUser');
                 if (response.data.statusCode === 200) {
-                    console.log(response.data.data.user.isVendor)
                     if (response.data.data.user.isVendor === true) {
                         setIsVendor(true);
                     }
