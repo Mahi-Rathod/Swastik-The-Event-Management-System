7923ba3c1dfb5247d6c80f8e4075a5011861ce83
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-05-20 17:59:51 +0530

    update

--- Server/NodeServer/src/controllers/user.controller.js
+++ Server/NodeServer/src/controllers/user.controller.js
@@ -89,7 +89,7 @@ const loginUser = asyncHandler(async(req, res)=>{
 
     const options = {
         httpOnly: true,
-        secure  : true,
+        secure  : false,
         sameSite: 'Strict',
         maxAge: 3600000,
     }
